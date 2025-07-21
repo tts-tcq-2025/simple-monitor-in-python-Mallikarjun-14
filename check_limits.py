a10712e6bb8197e3bfa3a591018bdfb841e2c8e7
Mallikarjun-14 <malluudagatti@gmail.com>
2025-07-21 15:04:18 +0530

    Update check_limits.py
    This change reduces cyclomatic complexity number(CCN) and makes the code easier to maintain.
    
    CCN in previous function is 6 because of the multiple or conditions and elif branches.
    Splitting the logic (updated code) into helper functions reduces it to 3 or less

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,26 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
+def is_temperature_ok(temperature):
+    if 0 <= temperature <= 45:
+        return True
     print('Temperature is out of range!')
     return False
-  elif soc < 20 or soc > 80:
+
+def is_soc_ok(soc):
+    if 20 <= soc <= 80:
+        return True
     print('State of Charge is out of range!')
     return False
-  elif charge_rate > 0.8:
+
+def is_charge_rate_ok(charge_rate):
+    if charge_rate <= 0.8:
+        return True
     print('Charge rate is out of range!')
     return False
 
-  return True
-
+def battery_is_ok(temperature, soc, charge_rate):
+    return is_temperature_ok(temperature) and is_soc_ok(soc) and is_charge_rate_ok(charge_rate)
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    assert(battery_is_ok(25, 70, 0.7) is True)
+    assert(battery_is_ok(50, 85, 0) is False)
+
